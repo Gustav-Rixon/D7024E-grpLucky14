46292d2ff54755c26efabf55e6479daa974271a7
Gustav-Rixon <48015764+Gustav-Rixon@users.noreply.github.com>
2022-09-07 11:43:13 +0200

    created random function
    we can now make random id's on the diffrent nodes

--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "math/rand"
+    "time"
+    "log"
+    "net"
+    "encoding/binary"
 )
+
+type Node struct{
+    ID  *KademliaID
+}
+
+func NewNode() Node {
+    Id := NewRandomKademliaID() 
+    return Node{Id} 
+}
+
+// Borrwed .)
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
+}
+
+func ip2int(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
 
 func main() {
 
-	sum := 0
-	for {
-		sum++
-		fmt.Println("deez" + fmt.Sprint(sum))
-	}
+    //node := NewNode()
+
+    s2 := rand.NewSource(int64(ip2int(GetOutboundIP())))
+    r1 := rand.New(s2)
+    //r := rand.Intn(256)
+    //r2 := rand.Intn(256)
+
+    for {
+        r := r1.Intn(256)
+        time.Sleep(2 * time.Second)
+        fmt.Println(r)
+    }
 }
