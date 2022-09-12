656460d8c9765492c8ffe7d318691e0a03bbb004
Fredde <48214381+Shambler34@users.noreply.github.com>
2022-09-12 15:12:35 +0200

    connected the connections
    hardcoded sending and listen functionality

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
-    "log"
-    "net"
-    "encoding/binary"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"time"
 )
 
-type Node struct{
-    ID  *KademliaID
+type Node struct {
+	ID *KademliaID
 }
 
 func NewNode() Node {
-    Id := NewRandomKademliaID() 
-    return Node{Id} 
+	Id := NewRandomKademliaID()
+	return Node{Id}
 }
 
 // Borrwed .)
 // Get preferred outbound ip of this machine
 func GetOutboundIP() net.IP {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
-    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    return localAddr.IP
+	return localAddr.IP
 }
 
-//converts ip address to int
+// converts ip address to int
 func ip2int(ip net.IP) uint32 {
 	if len(ip) == 16 {
 		return binary.BigEndian.Uint32(ip[12:16])
@@ -40,26 +40,88 @@ func ip2int(ip net.IP) uint32 {
 	return binary.BigEndian.Uint32(ip)
 }
 
-//Returns random number, used in Kademlia ID generation
-func getRandNum() int{    
-    r := rGen.Intn(256)
-    return r
+// Returns random number, used in Kademlia ID generation
+func getRandNum() int {
+	r := rGen.Intn(256)
+	return r
 }
 
-//Random number generator, use to get random numbers between nodes
+// Random number generator, use to get random numbers between nodes
 var rGen *rand.Rand
 
+// The node itself
+var node Node
+
 func main() {
-    //initialize randomization of ID
-    randSource := rand.NewSource(time.Now().UnixNano())
-    rGen = rand.New(randSource)
-    NodeID := NewRandomKademliaID()
+	//initialize randomization of ID
+	randSource := rand.NewSource(time.Now().UnixNano())
+	rGen = rand.New(randSource)
+	node.ID = NewRandomKademliaID()
 
+	if GetOutboundIP().String() == "172.18.0.2" {
+		listen()
+	} else {
+		send()
+	}
 
-    for {
-        time.Sleep(2 * time.Second)
-        
-        fmt.Println(NodeID)
-        
-    }
+	for {
+		time.Sleep(2 * time.Second)
+
+		fmt.Println(node.ID)
+
+	}
+}
+
+func listen() {
+	localAddress, err := net.ResolveUDPAddr("udp", GetOutboundIP().String()+":80")
+	if err != nil {
+		log.Fatal("bruh")
+	}
+
+	fmt.Println("Beginning to listen on ", localAddress)
+
+	connection, err := net.ListenUDP("udp", localAddress)
+	defer connection.Close()
+
+	for {
+		inputBytes := make([]byte, 4096)
+		_, senderAddr, _ := connection.ReadFromUDP(inputBytes)
+
+		fmt.Println("Received message from ", senderAddr, "\n Msg: ", string(inputBytes))
+	}
+}
+
+func send() {
+	dest_addr := "172.18.0.2"
+	port := ":80"
+
+	fmt.Printf("COMM: Broadcasting message to: %s%s\n", dest_addr, port)
+	localAddr, err := net.ResolveUDPAddr("udp", GetOutboundIP().String()+port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sendAddr, err := net.ResolveUDPAddr("udp", dest_addr+port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connection, err := net.DialUDP("udp", localAddr, sendAddr)
+	defer connection.Close()
+
+	if err != nil {
+		log.Fatal(connection, err)
+	}
+
+	message := []byte(string("hello from " + node.ID.String()[0:4] + " :))))"))
+	for {
+		fmt.Println("Sending message")
+		_, err = connection.Write(message)
+		if err != nil {
+			fmt.Println("Write in broadcast localhost failed", err)
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
