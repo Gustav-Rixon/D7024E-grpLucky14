f8a3d4d50e15dfda61c021741ab636f3e05a111d
Gustav-Rixon <Gustav.rixon@gmail.com>
2022-09-20 18:13:26 +0200

    Update commands.go

--- pkg/actions/commands.go
+++ pkg/actions/commands.go
@@ -10,13 +10,37 @@ import (
 
 func Commands() {
 	app := &cli.App{
-		Name:  "hello",
-		Usage: "say hello",
-		Action: func(c *cli.Context) error {
-			fmt.Println("hello")
-			return nil
+		Name:  "cli",
+		Usage: "do cli stuffff",
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "put",
+			Usage: "do the thing",
+			Action: func(c *cli.Context) error {
+				fmt.Println("put")
+				return nil
+			},
+		},
+		{
+			Name:  "get",
+			Usage: "do the thing",
+			Action: func(c *cli.Context) error {
+				fmt.Println("get")
+				return nil
+			},
+		},
+		{
+			Name:  "exit",
+			Usage: "do the thing",
+			Action: func(c *cli.Context) error {
+				fmt.Println("exit")
+				return nil
+			},
 		},
 	}
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
