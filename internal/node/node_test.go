c1464d791300f596537260f64c54ce742dbfb03f
Fredde <48214381+Shambler34@users.noreply.github.com>
2022-09-20 13:26:50 +0200

    Hash table functionality implemented
    simple key-value map solution for storing and getting values (strings)

---
+++ internal/node/node_test.go
@@ -0,0 +1,36 @@
+package node
+
+import (
+	"kademlia/internal/kademliaid"
+	"net"
+	"testing"
+)
+
+func TestHashTable(t *testing.T) {
+	CreateSelf(kademliaid.NewRandomKademliaID(), net.IPv4(0, 0, 0, 0)) // init table
+	StoreValue("test1")
+
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantExists bool
+		wantValue  string
+	}{
+		{"Check get existing value", args{getKey("test1")}, true, "test1"},
+		{"Check get nonexistent value", args{getKey("test2")}, false, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotExists, gotValue := GetValue(tt.args.key)
+			if gotExists != tt.wantExists {
+				t.Errorf("GetValue() gotExists = %v, want %v", gotExists, tt.wantExists)
+			}
+			if gotValue != tt.wantValue {
+				t.Errorf("GetValue() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+		})
+	}
+}
