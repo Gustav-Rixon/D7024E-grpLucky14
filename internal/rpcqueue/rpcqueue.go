3e8c44ef3c062cfec45087bce1416d1a387e5784
Fredde <48214381+Shambler34@users.noreply.github.com>
2022-10-04 17:02:44 +0200

    concept for concurrency

--- internal/rpcqueue/rpcqueue.go
+++ internal/rpcqueue/rpcqueue.go
@@ -3,45 +3,20 @@
 package rpcqueue
 
 import (
-	"kademlia/internal/contact"
-	"kademlia/internal/kademliaid"
+	"kademlia/internal/rpc"
 	"sync"
 )
 
-type Entry struct {
-	Channel chan string
-	RPCID   *kademliaid.KademliaID
-	Contact *contact.Contact
+var rpcQueue []rpc.RPC     // The queue for holding all RPC's
+var queueMutex sync.Mutex  // Mutex lock for modifying the queue
+var execSem sync.WaitGroup // Semaphore for counting all running execution threads
+
+// Adds an RPC request to the execution queue, begins running through the queue if semaphore count = 0
+func addToQueue() {
+
 }
 
-type RPCQueue struct {
-	lock    sync.Mutex //The queue should be protected by a mutex lock
-	content map[kademliaid.KademliaID]*Entry
-}
+func handleLoop() {
+	queueMutex.Lock()
 
-// Create a new RPCQueue
-func New() *RPCQueue {
-	return &RPCQueue{
-		content: make(map[kademliaid.KademliaID]*Entry),
-	}
-}
-
-// Add RPC to queue
-func (queue *RPCQueue) Add(RPCID *kademliaid.KademliaID, contact *contact.Contact) {
-	queue.content[*RPCID] = &Entry{RPCID: RPCID, Contact: contact, Channel: make(chan string)}
-}
-
-// Delete RPC from queue
-func (queue *RPCQueue) Delete(RPCID *kademliaid.KademliaID) {
-	delete(queue.content, *RPCID)
-}
-
-func (pool *RPCQueue) WithLock(f func()) {
-	pool.lock.Lock()
-	f()
-	pool.lock.Unlock()
-}
-
-func (pool *RPCQueue) GetEntry(rpcId *kademliaid.KademliaID) *Entry {
-	return pool.content[*rpcId]
 }
