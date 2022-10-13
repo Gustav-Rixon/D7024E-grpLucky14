9aae20c58ded26b463b032ce73486ada7d4f9117
Axelkarnebro <axekrn-7@student.ltu.se>
2022-10-13 14:56:07 +0200

    wow the RPC seems to actually kinda work???

--- internal/command/commands/put/put.go
+++ internal/command/commands/put/put.go
@@ -27,7 +27,7 @@ func (put *Put) Execute(node *node.Node) (string, error) {
 	for _, closeNode := range closestNodes {
 		node.Network.SendStoreMessage(node.ID, closeNode.Address, []byte(put.fileContent))
 	}
-	rpc := node.NewRPC("REFRESH"+key.String(), closestNodes[0].Address)
+	rpc := node.NewRPC("REFRESH "+key.String(), closestNodes[0].Address)
 	node.Network.SendRefreshMessage(&rpc)
 
 	return key.String(), nil
