a05619fcb79acbead277915515d1c57b10f022a0
Axelkarnebro <axekrn-7@student.ltu.se>
2022-10-17 14:36:24 +0200

    fixed error in storetest

--- internal/rpc/commands/store/store_test.go
+++ internal/rpc/commands/store/store_test.go
@@ -2,26 +2,27 @@ package store_test
 
 import (
 	"kademlia/internal/address"
-	"kademlia/internal/command/commands/put"
 	"kademlia/internal/kademliaid"
 	"kademlia/internal/node"
+	"kademlia/internal/rpc/commands/store"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExecute(t *testing.T) {
-	var s put.Put
+	var s store.Store
 	var options []string
 	var err error
 
 	// Should set file content if passed
 	options = []string{"this is some file content"}
 	fileContent := "this is some file content"
-	err = s.ParseOptions(options)
+	err = s.ParseOptions(&options)
 	assert.NoError(t, err)
 	node := node.Node{}
-	node.Init(address.New(""))
+	inAddr := "127.0.0.1:8888"
+	node.Init(address.New(inAddr))
 	s.Execute(&node)
 	assert.Equal(t, fileContent, node.DataStore.GetValue(kademliaid.NewKademliaID(&fileContent)))
 }
