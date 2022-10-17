966aee696ddff309c7450d30fe7556f8241a766e
Axelkarnebro <axekrn-7@student.ltu.se>
2022-10-17 14:17:36 +0200

    more tests

--- internal/datastore/datastore_test.go
+++ internal/datastore/datastore_test.go
@@ -43,3 +43,67 @@ func TestInsert(t *testing.T) {
 	assert.Equal(t, d.GetValue(kademliaid.NewKademliaID(&value)), "hello")
 
 }
+
+func TestGet(t *testing.T) {
+	var d datastore.DataStore
+
+	// Should be able to  get
+	d = datastore.New()
+	value := "hello"
+	contacts := &[]contact.Contact{}
+	d.Insert(value, contacts, nil, true)
+	assert.Equal(t, d.Get(kademliaid.NewKademliaID(&value)), "hello")
+
+	// Should not be able to get non-existent key
+	d = datastore.New()
+	value = "hello"
+	assert.Equal(t, d.Get(kademliaid.NewKademliaID(&value)), "")
+}
+
+func TestRefresh(t *testing.T) {
+	var d datastore.DataStore
+
+	// Should be able to  get
+	d = datastore.New()
+	value := "hello"
+	contacts := &[]contact.Contact{}
+	d.Insert(value, contacts, nil, true)
+	assert.Equal(t, d.Refresh(kademliaid.NewKademliaID(&value)), "cool")
+
+	// Should not be able to get non-existent key
+	d = datastore.New()
+	value = "hello"
+	assert.Equal(t, d.Refresh(kademliaid.NewKademliaID(&value)), "")
+}
+
+func TestGetForget(t *testing.T) {
+	var d datastore.DataStore
+
+	// Should be able to  get
+	d = datastore.New()
+	value := "hello"
+	contacts := &[]contact.Contact{}
+	d.Insert(value, contacts, nil, false)
+	assert.Equal(t, d.GetForget(kademliaid.NewKademliaID(&value)), false)
+
+	// Should not be able to get non-existent key
+	d = datastore.New()
+	value = "hello"
+	assert.Equal(t, d.GetForget(kademliaid.NewKademliaID(&value)), true)
+}
+
+func TestSetForget(t *testing.T) {
+	var d datastore.DataStore
+
+	// Should be able to  get
+	d = datastore.New()
+	value := "hello"
+	contacts := &[]contact.Contact{}
+	d.Insert(value, contacts, nil, false)
+	assert.Equal(t, d.SetForget(kademliaid.NewKademliaID(&value), false), "cool")
+
+	d = datastore.New()
+
+	assert.Equal(t, d.SetForget(kademliaid.NewKademliaID(&value), true), "")
+
+}
