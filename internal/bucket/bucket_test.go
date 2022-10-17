f438ffe31371061d3c25ffb070588327ad6754f0
Axelkarnebro <axekrn-7@student.ltu.se>
2022-10-17 14:25:37 +0200

    more testing

--- internal/bucket/bucket_test.go
+++ internal/bucket/bucket_test.go
@@ -1,7 +1,10 @@
 package bucket_test
 
 import (
+	"kademlia/internal/address"
 	"kademlia/internal/bucket"
+	"kademlia/internal/contact"
+	"kademlia/internal/kademliaid"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,4 +15,36 @@ func TestNewBucket(t *testing.T) {
 	b := bucket.NewBucket()
 	assert.NotNil(t, b)
 	assert.Equal(t, b.Len(), 0)
+}
+
+func TestAddContact(t *testing.T) {
+	b := bucket.NewBucket()
+	key := kademliaid.NewRandomKademliaID()
+	inAddr := "127.0.0.1:8888"
+	adr := address.New(inAddr)
+	b.AddContact(contact.NewContact(key, adr))
+	assert.NotNil(t, b)
+	assert.Equal(t, b.Len(), 1)
+}
+
+func TestGetContactAndCalcDistance(t *testing.T) {
+	b := bucket.NewBucket()
+	key := kademliaid.NewRandomKademliaID()
+	inAddr := "127.0.0.1:8888"
+	adr := address.New(inAddr)
+	b.AddContact(contact.NewContact(key, adr))
+	key2 := kademliaid.NewRandomKademliaID()
+	c := b.GetContactAndCalcDistance(key2)
+	assert.NotNil(t, c)
+}
+
+func TestGetContactAndCalcDistanceNoRequestor(t *testing.T) {
+	b := bucket.NewBucket()
+	key := kademliaid.NewRandomKademliaID()
+	inAddr := "127.0.0.1:8888"
+	adr := address.New(inAddr)
+	b.AddContact(contact.NewContact(key, adr))
+	key2 := kademliaid.NewRandomKademliaID()
+	c := b.GetContactAndCalcDistanceNoRequestor(key2, key)
+	assert.NotNil(t, c)
 }
